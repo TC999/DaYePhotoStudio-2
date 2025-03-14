7ad53590fd7e78b53c6a6e99ddbf7af4e1bbef72
unknown <darkmatter2048@outlook.com>
2025-03-14 17:35:52 +0800

    其他

--- pages/donate.py
+++ pages/donate.py
@@ -11,4 +11,12 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title("捐赠")
-st.write("这是一个简单的图像处理软件示例。")
+st.markdown("""
+### 支持DaYe的开源工作 ❤️
+- [爱发电](https://afdian.com/a/donate_daye)
+- [微信赞赏](https://dyblog.online/assets/wechatpay.jpg) *(China)*
+- [B站充电](https://space.bilibili.com/1847808902?from=dyblog.online)
+### 慈善捐赠❤️
+- [哔哩哔哩公益](https://love.bilibili.com/?from=dyblog.online)
+    
+""")
--- pages/settings.py
+++ pages/settings.py
@@ -3,3 +3,4 @@
 import streamlit as st
 
 st.header("这是设置页面")
+st.write("敬请期待>_<")
